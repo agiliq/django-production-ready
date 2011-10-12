d7e0caade9e19342619a9f73ffd3fd580ac844d7
Shabda Raaj <shabda@agiliq.com>
2011-10-12 13:16:10 +0530

    Pass all the checks and 404 and 500 templates

--- settings.py
+++ settings.py
@@ -95,6 +95,7 @@ STATICFILES_FINDERS = (
 SECRET_KEY = '5k2hj2#70p0vxez)jcw@0e#wvt^=_6s9mt317y-n7!=gh=l$s&'
 
 # List of callables that know how to import templates from various sources.
+
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
@@ -111,10 +112,14 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'ready.urls'
 
+import os
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(SITE_ROOT, "templates")
 )
 
 INSTALLED_APPS = (
